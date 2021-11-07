e09983efa6aab08c62ece568faf1d733e62a93a5
Axiu MAO <59108793+Max-1234-hub@users.noreply.github.com>
2021-11-07 20:29:15 +0800

    Add files via upload

---
+++ conf/global_settings.py
@@ -0,0 +1,34 @@
+""" configurations for this project
+
+author baiyu
+"""
+
+from datetime import datetime
+
+#directory to save weights file
+CHECKPOINT_PATH = 'checkpoint'
+
+#total training epoches
+EPOCH = 200
+MILESTONES = [0, 40, 80]
+
+#initial learning rate
+#INIT_LR = 0.1
+
+DATE_FORMAT = '%A_%d_%B_%Y_%Hh_%Mm_%Ss'
+#time of we run the script
+TIME_NOW = datetime.now().strftime(DATE_FORMAT)
+
+#tensorboard log dir
+LOG_DIR = 'runs'
+
+#save weights file per SAVE_EPOCH epoch
+SAVE_EPOCH = 10
+
+
+
+
+
+
+
+
